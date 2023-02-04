2df415e917f751e8aba1ff3d80040b2ee9d8b22f
Ian McCall <imccall@clemson.edu>
2023-02-04 17:36:28 -0500

    added screen

--- RythmGame/main.py
+++ RythmGame/main.py
@@ -1,15 +1,16 @@
-import pygame
+import pygame, Draw
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main():
-
+    screen = Draw.Draw()
     run = True
     while(run):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        screen.draw(WIN)
 
     pygame.quit()
 
