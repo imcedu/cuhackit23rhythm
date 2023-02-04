b5f76f1faa4a945b319b6a036d9e94b9d3ca2958
sarasu2 <73024712+sarasu2@users.noreply.github.com>
2023-02-04 16:07:23 -0500

    Created JoyStick.py

---
+++ RythmGame/JoyStick.py
@@ -0,0 +1,34 @@
+import pygame
+from Position import Position
+
+RED = (255, 0, 0)
+GRID_SQUARE_SIZE = 70
+
+class JoyStick(Position):
+    def __init__(self, posX, posY, color):
+        super().__init__(posX, posY)
+        self.color = color
+
+    def move_stick(self, WIN, keys_pressed):
+
+        if keys_pressed[pygame.K_a]: #LEFT
+            self.posX = self.posX - GRID_SQUARE_SIZE
+
+        if keys_pressed[pygame.K_d]: #RIGHT
+            self.posX = self.posX + GRID_SQUARE_SIZE
+        
+        if keys_pressed[pygame.K_w]: #UP
+            self.posY = self.posY - GRID_SQUARE_SIZE
+
+        if keys_pressed[pygame.K_s]: #DOWN
+            self.posY = self.posY + GRID_SQUARE_SIZE
+
+        # Draw the joystick in the new location
+        pygame.draw.rect(WIN, RED, pygame.Rect(self.posX, self.posY, GRID_SQUARE_SIZE, GRID_SQUARE_SIZE))
+
+
+    def reset_stick(self, start_index):
+        self.posX = start_index
+        self.posY = start_index
+        
+
