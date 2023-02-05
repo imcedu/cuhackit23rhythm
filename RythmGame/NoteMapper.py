774d4938804294bcbcfc220b16ff6491a20e9350
Ian McCall <imccall@clemson.edu>
2023-02-04 22:06:47 -0500

    Added beatmap parser

---
+++ RythmGame/NoteMapper.py
@@ -0,0 +1,13 @@
+class NoteMapper(object):
+    def __init__(self):
+        self.map = []
+        
+        with open('RythmGame/beatmap.txt', 'r') as file:
+            line = file.readline()
+            while line:
+                line = file.readline()
+                if not line:
+                    break
+                self.map.append((line[0], line[1:7]))
+
+
