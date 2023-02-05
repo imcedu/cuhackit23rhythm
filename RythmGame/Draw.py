b384b865ac9c58f8f7ef68cc4da369699a702a7a
Ian McCall <imccall@clemson.edu>
2023-02-05 08:28:23 -0500

    added Note assets

--- RythmGame/Draw.py
+++ RythmGame/Draw.py
@@ -3,6 +3,22 @@ import pygame, os, Note, JoyStick
 GRID_SQUARE_SIZE = 70
 GAMESCREEN_IMAGE = pygame.image.load(
     os.path.join('RythmGame/graphics', 'gamescreen.png'))
+NOTE_C_UP = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'north.png'))
+NOTE_C_DOWN = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'south.png'))
+NOTE_C_LEFT = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'west.png'))
+NOTE_C_RIGHT = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'east.png'))
+NOTE_D_UPLEFT = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'northwest.png'))
+NOTE_D_UPRIGHT = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'northeast.png'))
+NOTE_D_DNRIGHT = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'southeast.png'))
+NOTE_D_DNLEFT = pygame.image.load(
+    os.path.join('RythmGame/asteroids', 'southwest.png'))
 QUIT_TEXT = pygame.image.load(os.path.join('RythmGame/graphics', 'quit_instructions.png'))
 
 RED = (255, 0, 0)
@@ -36,8 +52,45 @@ class Draw(object):
         #loop through all of the objects in the queue and draw based on their type
         for o in self.drawQueue:
             if isinstance(o, Note.Note):
-                #TODO: Replace placeholder shape with correct pngs (will make use of both position and direction)
-                pygame.draw.rect(WIN, BLUE, pygame.Rect(o.posX, o.posY, GRID_SQUARE_SIZE, GRID_SQUARE_SIZE))
+                match o.direction:
+                    # C_UP = 0,
+                    case 0:
+                        image = NOTE_C_UP
+                        pass
+
+                    # C_DOWN = 1,
+                    case 1:
+                        image = NOTE_C_DOWN
+                        pass
+
+                    # C_LEFT = 2,
+                    case 2:
+                        image = NOTE_C_LEFT
+                        pass
+                    
+                    # C_RIGHT = 3,
+                    case 3:
+                        image = NOTE_C_RIGHT
+                        pass
+
+                    # D_UPLEFT = 4,
+                    case 4:
+                        image = NOTE_D_UPLEFT
+
+                    # D_UPRIGHT = 5,
+                    case 5:
+                        image = NOTE_D_UPRIGHT
+
+                    # D_DNRIGHT = 6,
+                    case 6:
+                        image = NOTE_D_DNRIGHT
+
+                    # D_DNLEFT = 7,
+                    case 7:
+                        image = NOTE_D_DNLEFT
+
+                #image = pygame.transform.scale_by(image, 30)
+                WIN.blit(image, (o.posX, o.posY))
             elif isinstance(o, JoyStick.JoyStick):
                 #TODO: Replace placeholder shape with correct png
                 pygame.draw.rect(WIN, RED, pygame.Rect(o.posX, o.posY, GRID_SQUARE_SIZE, GRID_SQUARE_SIZE))
